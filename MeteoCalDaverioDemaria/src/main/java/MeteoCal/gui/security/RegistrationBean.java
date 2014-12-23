055c3e5d37424ddbb8cc3e405ff46acd20e45248
matteo_daverio <matteo.daverio@mail.polimi.it>
2014-12-23 14:43:56 +0100

    registration page very cool!

---
+++ MeteoCalDaverioDemaria/src/main/java/MeteoCal/gui/security/RegistrationBean.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MeteoCal.gui.security;
+
+
+/**
+ *
+ * @author Matteo
+ */
+public class RegistrationBean {
+   
+/*    private UserManager um;
+    private User user;
+    
+    public RegistrationBean() {
+    }
+
+    public User getUser() {
+        if (user == null) {
+            user = new User();
+        }
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String register() {
+        um.save(user);
+        return "user/home?faces-redirect=true";
+    }
+*/
+} 
