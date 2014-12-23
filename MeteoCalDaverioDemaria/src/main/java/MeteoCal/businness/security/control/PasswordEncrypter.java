dbb47896a9045cd8386844672d2560af553c7bc8
ValerioDeMaria <valerio.demaria@mail.polimi.it>
2014-12-23 17:20:22 +0100

    packages BOUNDARY, CONTROL and ENTITY created

---
+++ MeteoCalDaverioDemaria/src/main/java/MeteoCal/businness/security/control/PasswordEncrypter.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MeteoCal.businness.security.control;
+
+import MeteoCal.business.security.entity.User;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *CREATED TO ENCRYPT PASSWORD ONCE IT HAS TO BE SET IN THE ENTITY USER
+ * 
+ * @author DeMaria
+ */
+public class PasswordEncrypter {
+    
+        public static String encryptPassword(String password) {
+        String encPass = null;
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(password.getBytes("UTF-8"));
+            BigInteger bigInt = new BigInteger(1, hash);
+            encPass = bigInt.toString(16);
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return encPass;
+    }
+}
