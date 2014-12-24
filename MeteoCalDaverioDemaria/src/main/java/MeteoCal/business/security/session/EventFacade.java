76083e606bcbfc124e085f511d59a16509d2a7ca
ValerioDeMaria <valerio.demaria@mail.polimi.it>
2014-12-24 14:43:45 +0100

    Package "session" added

---
+++ MeteoCalDaverioDemaria/src/main/java/MeteoCal/business/security/session/EventFacade.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MeteoCal.business.security.session;
+
+import MeteoCal.business.security.entity.Event;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author DeMaria
+ */
+@Stateless
+public class EventFacade extends AbstractFacade<Event> {
+    @PersistenceContext(unitName = "com.mycompany_MeteoCalDaverioDemaria_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public EventFacade() {
+        super(Event.class);
+    }
+    
+}
