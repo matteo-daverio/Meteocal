6ffe6fbfbeddf7cfc095ec7ab1e01f97ce216935
ValerioDeMaria <valerio.demaria@mail.polimi.it>
2015-01-24 12:25:06 +0100

    test improving

--- MeteoCalDaverioDemaria/src/test/java/MeteoCal/business/security/boundary/EventManager_MokitoTest.java
+++ MeteoCalDaverioDemaria/src/test/java/MeteoCal/business/security/boundary/EventManager_MokitoTest.java
@@ -6,16 +6,21 @@
 package MeteoCal.business.security.boundary;
 
 import MeteoCal.business.security.entity.Event;
+import MeteoCal.business.security.entity.Notification;
 import MeteoCal.business.security.entity.Users;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  *
@@ -27,8 +32,10 @@ public class EventManager_MokitoTest {
     
     @Before
     public void setUp() {
-        cut = new EventManager();
-        cut.setEm(mock(EntityManager.class));
+        cut= mock(EventManager.class);
+        
+         //cut = new EventManager();
+        //cut.setEm(mock(EntityManager.class));
     }
     
     @After
@@ -36,21 +43,49 @@ public class EventManager_MokitoTest {
     }
 
     @Test
-    public void newEventShouldSavedOnce(){
+    public void verificationAddRemoveEvent(){
         
-        Event e =new Event();
-        e.setOutdoor(Boolean.TRUE);
-        cut.addEvent(e);
+        System.out.println("verificationAddRemoveEvent");
         
-        //assertThat(cut.isIndoor(e),false);
-//          try {
-//             // cut.save(newUser);
-//          } catch (Exception ex) {
-//              Logger.getLogger(UserManager_MokitoTest.class.getName()).log(Level.SEVERE, null, ex);
-//          }
+        Event e1 =new Event();
+        Event e2 =new Event();
+        
+        cut.addEvent(e1);
+        cut.removeEvent(e1);
+        cut.addEvent(e2);
+        cut.removeEventByID(e2);
+        
+        verify(cut).addEvent(e1);
+        verify(cut).removeEvent(e1);
+        verify(cut).addEvent(e2);
+        verify(cut).removeEventByID(e2);
+        
+    }
+    @Test 
+    public void loadCalendar() throws Exception {
+    
+        System.out.println("loadCalendar");
+        
+        EventManager testClass = Mockito.mock(EventManager.class);
+   
+        NotificationManager nm = Mockito.mock(NotificationManager.class);
+        
+        UserManager um =Mockito.mock(UserManager.class);
+        
+        
+        
+        Users user = new Users();
+        Event e =new Event();  
+        Notification n =new Notification(e,user,true);
+        
+        um.save(user);
+        testClass.addEvent(e);
+        nm.addNotification(n);
+        
+        List<Event> someList =new ArrayList<Event>();
+        
+        Mockito.when(testClass.loadCalendar(user)).thenReturn(someList);
           
-        //assertThat(newUser.getGroupName(),is(Group.USERS));
-       // verify(cut.em,times(1)).persist(newUser);
-    } 
+    }
     
 }
